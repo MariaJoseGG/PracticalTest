473dda4d31f216efed0513dbc231cf92faa5912a
MariaJoseGG <mgiraldogu@unal.edu.co>
2023-11-27 23:43:42 -0500

    The reducer logic is separated into other files

--- src/TaskManagement/components/TaskList.jsx
+++ src/TaskManagement/components/TaskList.jsx
@@ -1,87 +1,40 @@
 import { useReducer } from 'react'
 import '../styles/TaskList.css'
 import { TaskForm } from './TaskForm'
-
-// Action types
-const ADD_TASK = '[TASKS] Add task'
-const COMPLETE_TASK = '[TASKS] Complete task'
-const DELETE_TASK = '[TASKS] Delete task'
-
-// Reducer actions
-const taskReducer = (state = [], action = {}) => {
-  switch (action.type) {
-    case ADD_TASK:
-      // The current task list, plus the new task that has been dispatched
-      return [...state, action.payload]
-
-    case COMPLETE_TASK:
-      // The status of the task is changed to completed
-      return state.map(task => {
-        if (task.id === action.payload) {
-          return {
-            ...task,
-            completed: !task.completed
-          }
-        } else {
-          return task
-        }
-      })
-
-    case DELETE_TASK:
-      return state.filter(task => task.id !== action.payload)
-
-    default:
-      return state
-  }
-}
+import { addTask, completeTask, deleteTask } from '../functions/taskActions'
+import { taskReducer } from '../functions/taskReducer'
 
 export const TaskList = () => {
 
   const [taskState, dispatch] = useReducer(taskReducer, [])
 
   // Add new task
-  const addTask = (task) => {
+  const handleAddTask = (task) => {
     const newTask = {
       id: new Date().getTime(),
       task: task,
       completed: false
     }
 
-    const action = {
-      type: ADD_TASK,
-      payload: newTask
-    }
-
-    dispatch(action)
+    dispatch(addTask(newTask))
   }
 
   // Marking or unmarking a task as completed
-  const checkTask = ({ id }) => {
-    // Only the task id is needed to change the task status
-    const action = {
-      type: COMPLETE_TASK,
-      payload: id
-    }
-
-    dispatch(action)
+  const handleCompleteTask = ({ id }) => {
+    dispatch(completeTask(id))
   }
 
   // Delete task
-  const deleteTask = ({ id }) => {
-    const action = {
-      type: DELETE_TASK,
-      payload: id
-    }
-
-    dispatch(action)
+  const handleDeleteTask = ({ id }) => {
+    dispatch(deleteTask(id))
   }
 
   return (
-    <>
+    <div className='container'>
       <h1 className='text-center'>Lista de Tareas</h1>
 
       <TaskForm
-        addTask={addTask}
+        addTask={handleAddTask}
       />
 
       <hr />
@@ -96,12 +49,12 @@ export const TaskList = () => {
                 <input
                   type="checkbox"
                   value={item.finalizada}
-                  onChange={() => checkTask(item)}
+                  onChange={() => handleCompleteTask(item)}
                 />
 
                 <button
                   className="btn btn-danger"
-                  onClick={() => deleteTask(item)}>
+                  onClick={() => handleDeleteTask(item)}>
                   Borrar
                 </button>
               </div>
@@ -109,6 +62,6 @@ export const TaskList = () => {
           )
         })}
       </ul>
-    </>
+    </div>
   )
 }
---
+++ src/TaskManagement/functions/taskReducer.js
@@ -0,0 +1,32 @@
+// Action types
+export const ADD_TASK = '[TASKS] Add task'
+export const COMPLETE_TASK = '[TASKS] Complete task'
+export const DELETE_TASK = '[TASKS] Delete task'
+
+// Reducer actions
+export const taskReducer = (state = [], action = {}) => {
+  switch (action.type) {
+    case ADD_TASK:
+      // The current task list, plus the new task that has been dispatched
+      return [...state, action.payload]
+
+    case COMPLETE_TASK:
+      // The status of the task is changed to completed
+      return state.map(task => {
+        if (task.id === action.payload) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        } else {
+          return task
+        }
+      })
+
+    case DELETE_TASK:
+      return state.filter(task => task.id !== action.payload)
+
+    default:
+      return state
+  }
+}
